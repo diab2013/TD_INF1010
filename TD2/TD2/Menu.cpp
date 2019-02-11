19a1512db9ca86530f8d1fabbf531be036f00b2a
Charlyboy1111 <44471752+Charlyboy1111@users.noreply.github.com>
2019-02-10 23:42:06 -0500

    Commentaires sauf resto

--- TD2/TD2/Menu.cpp
+++ TD2/TD2/Menu.cpp
@@ -6,13 +6,22 @@
 */
 #include "Menu.h"
 
-//constructeurs
+/*
+* Constructeur par défaut de la classe Menu
+* Crée une nouvelle liste de plat vide avec une capacité de MAXPLAT
+* Le nombre de plat est zéro et le type de menu est Matin
+*/
 Menu::Menu() {
 	capacite_ = MAXPLAT;
 	nbPlats_ = 0;
 	type_ = Matin;
 }
 
+/*
+* Constructeur par paramètre de la classe Menu
+* Initialise les attributs à des valeurs passées en paramètres
+* Aussi, fait appelle à la méthode lireMenu
+*/
 Menu::Menu(string fichier, TypeMenu type) {
 	capacite_ = MAXPLAT;
 	nbPlats_ = 0;
@@ -22,6 +31,11 @@ Menu::Menu(string fichier, TypeMenu type) {
 	lireMenu(fichier);
 }
 
+/*
+* In: Variable de type Menu
+* Creer un objet Menu ayant les memes attributs que ce menu-ci
+* et ajoute ses plats dans le vecteur de plats
+*/
 Menu::Menu(const Menu & menu)
 	: capacite_(menu.capacite_), nbPlats_(menu.nbPlats_), type_(menu.type_){
 	for (int i = 0; i < menu.nbPlats_; i++) {
@@ -29,7 +43,7 @@ Menu::Menu(const Menu & menu)
 	}
 }
 
-//destructeur
+//destructeur de la classe
 Menu::~Menu() {
 	// A MODIFIER
 	for (int i = 0; i < nbPlats_; i++)
@@ -37,16 +51,27 @@ Menu::~Menu() {
 	//delete[] listePlats_;
 }
 
-//getters
+/*
+* Getter de la variable nbPlats_
+* Return l'attributs nbPlats_ de la classe
+*/
 int Menu::getNbPlats() const {
 	return nbPlats_;
 }
 
+/*
+* Methode qui return le vecteur de la liste de plats
+*/
 vector<Plat*> Menu::getListePlat() const{
 	return { listePlats_ };
 }
 
-//méthode en plus
+/*
+* In: Nom du fichier de lecture
+* Méthode qui permet de lire le fichier, passé en paramètre, contenant les menus selon chaque moment
+* de la journée et les tables du restaurant
+* return true/false si l'ouverture du fichier à été possible
+*/
 bool Menu::lireMenu(const string& fichier) {
 	ifstream file(fichier, ios::in);
 	if (!file) {
@@ -119,6 +144,11 @@ bool Menu::lireMenu(const string& fichier) {
 	}
 }
 
+/*
+* Méthode qui permet de trouver le plat le moins cher dans 
+*la liste dynamique de plat. 
+* return un pointeur vers le plat le moins cher
+*/
 Plat * Menu::trouverPlatMoinsCher() const{
 	Plat minimum(*listePlats_[0]);
 	int found = -1;
@@ -131,11 +161,24 @@ Plat * Menu::trouverPlatMoinsCher() const{
 	return listePlats_[found];
 }
 
+/*
+* In: plat a ajouter
+* Méthode qui permet d'ajouter un plat à la liste, mais cette fois-ci en passant le nom, le
+* montant et le cout en paramètre
+* Les paramètres sont utilisés pour créer un plat et ensuite le passer à la première méthode
+* ajouterPlat pour l'ajouter à la liste
+*/
 void Menu::ajouterPlat(const Plat& plat){
 	listePlats_.push_back(new Plat(plat));
 	nbPlats_++;
 }
 
+/*
+* In: nom du plat a trouver
+* Méthode qui permet de trouver un certain plat dans la liste dynamique
+* de plat selon un variable nom passée en paramètre
+* return un pointeur vers le plat s'il a été trouver, sinon return nullptr
+*/
 Plat* Menu::trouverPlat(const string& nom) const {
 	for (int i = 0; i < nbPlats_; i++) {
 		if (listePlats_[i]->getNom() == nom)
@@ -144,6 +187,12 @@ Plat* Menu::trouverPlat(const string& nom) const {
 	return nullptr;
 }
 
+/*
+* In/out: stream de l'output
+* In: Menu a afficher
+* Out: string contenant les informations du menu
+* Surcharge de l'operateur <<. Remplace la methode d'affichage.
+*/
 ostream & operator<<(ostream & o, const Menu & menu){
 	switch (menu.type_) {
 		case Matin: 
@@ -163,6 +212,11 @@ ostream & operator<<(ostream & o, const Menu & menu){
 	return o;
 }
 
+/*
+* In: Plat a ajouter
+* Out: Return la reference au menu modifie
+* Surcharge de l'operateur +=. Ajoute un plat dans la liste de plat.
+*/
 Menu& Menu::operator+=(Plat* plat){
 	if (nbPlats_ < capacite_) {
 		listePlats_.push_back(plat);
@@ -173,6 +227,11 @@ Menu& Menu::operator+=(Plat* plat){
 	return *this;
 }
 
+/*
+* In: Menu a copier
+* Out: Return la reference au menu copie
+* Surcharge de l'operateur =. Copie le menu dans un autre objet.
+*/
 Menu& Menu::operator=(Menu & menu){
 	capacite_ = menu.capacite_;
 	nbPlats_ = menu.nbPlats_;
--- TD2/TD2/Table.cpp
+++ TD2/TD2/Table.cpp
@@ -7,7 +7,12 @@
 
 #include "Table.h"
 
-//constructeurs
+/*
+* Constructeur par défaut de la classe Table
+* Crée une nouvelle liste de plat vide avec une capacité de MAXCAP
+* Le nombre de plat est zéro, le id de la table est -1, le nombre de places à 1
+* et la table n'est pas occupée
+*/
 Table::Table() {
 	capacite_ = MAXCAP;
 	nbPlats_ = 0;
@@ -16,6 +21,10 @@ Table::Table() {
 	nbClientsATable_ = 0;
 }
 
+/*
+* Constructeur par paramétres de la classe Table
+* Initialise les attributs à des valeurs passées en paramètres
+*/
 Table::Table(int id, int nbPlaces) {
 	capacite_ = MAXCAP;
 	nbPlats_ = 0;
@@ -24,38 +33,63 @@ Table::Table(int id, int nbPlaces) {
 	nbClientsATable_ = 0;
 }
 
-//destructeur
+//destructeur de la classe
 Table::~Table() {
 	//A MODIFIER
 	libererTable();
 }
 
-//getters
+/*
+* Getter de la variable id
+* Return l'attribut id_ de la classe
+*/
 int Table::getId() const {
 	return id_;
 }
 
+/*
+* Getter de la variable nbPlaces_
+* Return l'attribut nbPlaces_ de la classe
+*/
 int Table::getNbPlaces() const {
 	return nbPlaces_;
 }
 
+/*
+* Getter de la variable nbClientsATable_
+* Return l'attribut nbClientsATable_ de la classe
+*/
 int Table::getnbClientATable() const{
 	return nbClientsATable_;
 }
 
+/*
+* Return l'attribut nbPlaces_ avec une valeur de 0
+*/
 bool Table::estPleine() const {
 	return nbPlaces_==0;
 }
 
+/*
+* Return 1 si nbClientsATable n'a pas la valeur 0
+* Méthode qui permet de placer la table comme occupee.
+*/
 bool Table::estOccupee() const{
 	return nbClientsATable_!=0;
 }
 
-//setters
+/*
+* In: Id de la table
+* Méthode qui set le id à la valeur passée en paramètre
+*/
 void Table::setId(int id) {
 	id_ = id;
 }
 
+/*
+* Méthode qui permet de liberer une table et la reset
+* Supprime la commande actuelle et met la table comme libre
+*/
 void Table::libererTable() {
 	nbPlaces_ += nbClientsATable_;
 	nbClientsATable_ = 0;
@@ -63,12 +97,22 @@ void Table::libererTable() {
 	nbPlats_ = 0;
 }
 
+/*
+* In: nombre de client a placer
+* Set le nombre de client a la table au nombre recu en parametre
+* et fait la difference entre le nombre de clients et le nombre de place
+*/
 void Table::placerClient(int nbClients) {
 	nbClientsATable_ = nbClients;
 	nbPlaces_ -= nbClients;
 }
 
-//autres methodes
+/*
+* In: Plat a commande
+* Méthode qui permer de commande un plat pour les clients assis à la table
+* Si le nombre de plats commander n'est pas dépassé, ajoute le plat passé
+* en paramètre à la liste de commande
+*/
 void Table::commander(Plat* plat) {
 	if (nbPlats_ == capacite_) {
 		capacite_ *= 2;
@@ -77,6 +121,11 @@ void Table::commander(Plat* plat) {
 	nbPlats_++;
 }
 
+/*
+* Méthode qu perment de calculer le chiffre d'affaire fait par la liste de commande
+* de la table
+* return le chiffre d'affaire après le calcul
+*/
 double Table::getChiffreAffaire() const {
 	double chiffre = 0;
 	for (int i = 0; i < nbPlats_; i++) {
@@ -85,7 +134,12 @@ double Table::getChiffreAffaire() const {
 	return chiffre;
 }
 
-//override
+/*
+* In/out: stream de l'output
+* In: Table a afficher
+* Out: String contenant les informations de la table
+* Surcharge de l'operateur <<. Remplace la methode d'affichage.
+*/
 ostream& operator<<(ostream & o, const Table & table){
 	cout << "La table numero " << table.getId();
 	if (table.estOccupee()) {
