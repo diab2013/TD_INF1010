a72b84705706198d6587d453393da994575d553a
Charlyboy1111 <44471752+Charlyboy1111@users.noreply.github.com>
2019-04-08 00:32:54 -0400

    commentaires pt3

--- TD5/TD5/Foncteur.h
+++ TD5/TD5/Foncteur.h
@@ -14,8 +14,12 @@ using namespace std;
 
 class FoncteurPlatMoinsCher {
 public:
+    //Constructeur
 	FoncteurPlatMoinsCher() {};
-
+	/*
+	* In: 2 objets de pair d'un string et un pointeur de Plat
+	* Compare le prix de ces deux plats et renvoie un "true" ou "false" si le prix du premier est plus petit que le second
+	*/
 	bool operator()(pair<string, Plat*>plat1, pair<string, Plat*>plat2) { //a modifier
 		return { ((plat1).second->getPrix()) < ((plat2).second->getPrix()) };
 	}
@@ -24,8 +28,16 @@ public:
 
 class FoncteurIntervalle {
 public:
+	/*
+	* In: bornes superieur et inferieur
+	* Constructeur par parametre
+	*/
 	FoncteurIntervalle(double borneInf, double borneSup) { borneInf_ = borneInf;
 															borneSup_ = borneSup; };
+	/*
+	* In: objet de pair d'un string et un pointeur de Plat
+	* Retourne "true" ou "false" si le prix du plat est entre les bornes superieur et inferieur
+	*/
 	bool operator() (pair<string, Plat*> p) {
 		return { (p.second->getPrix() >= borneInf_) && (p.second->getPrix() <= borneSup_) };
 	};
